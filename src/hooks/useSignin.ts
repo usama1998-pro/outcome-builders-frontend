d0854b0ac21a4b064e0ad62ea1e2931dc337a2c4
usama1998-pro <154095855+usama1998-pro@users.noreply.github.com>
2025-09-18 23:35:23 +0500

    type error fixed

--- src/hooks/useSignin.ts
+++ src/hooks/useSignin.ts
@@ -5,6 +5,7 @@ import { useMutation } from "@tanstack/react-query";
 import  { signin } from "../api/auth";
 import { AuthResponse, SignupPayload } from "../types/auth";
 import { useLogin } from "../hooks/useAuth";
+import { da } from "zod/v4/locales";
 
 
 export function useSignin() {
@@ -14,9 +15,15 @@ export function useSignin() {
   return useMutation<AuthResponse, Error, SignupPayload>({
     mutationFn: signin,
     onSuccess: (data) => {
-      const token = data?.data?.token;
-      if (token) {
-        login(token); // ✅ delegate to useLogin
+
+      if(!data?.data){
+        console.error("No data in signin response");
+        return;
+      }
+
+      const tokenData = data?.data as { token: string } | undefined;
+      if (tokenData && tokenData.token) {
+        login(tokenData.token); // ✅ delegate to useLogin
       } else {
         console.error("Signin response did not include a token");
       }
